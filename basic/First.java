45a3dd5f09906c28f5d40864ac5155b18027a6ec
the-pandey-ji <pandey143sonu@gmail.com>
2023-05-12 01:44:35 +0530

    switch statement used

--- basic/First.java
+++ basic/First.java
@@ -1,8 +1,23 @@
 package basic;
 
+import java.util.Scanner;
+
 public class First{
     public static void main(String[] args) {
         System.out.println("Journey Started");
         System.out.println("'checking single comma'");
+        Scanner sc =new Scanner(System.in);
+        String button = sc.next();
+        char btt = button.charAt(0);
+        switch (btt) {
+            case 'a':
+                System.out.println("first case");
+                break;
+        
+            default:
+            System.out.println("default case");
+                break;
+        }
+
     }
 }
