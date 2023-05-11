d4b29a0e2d129dfa677ce3602050a433d695a214
the-pandey-ji <pandey143sonu@gmail.com>
2023-05-12 01:31:41 +0530

    single coma not working on string

--- basic/First.java
+++ basic/First.java
@@ -3,5 +3,6 @@ package basic;
 public class First{
     public static void main(String[] args) {
         System.out.println("Journey Started");
+        System.out.println("'checking single comma'");
     }
 }
