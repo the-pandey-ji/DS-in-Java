d0f8b0e64767e27fb5401a33fb87d4d0fe9ce774
the-pandey-ji <pandey143sonu@gmail.com>
2023-06-28 17:59:59 +0530

    error resolved

---
+++ Array.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Array{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Array file testing");
+
+        sc.close();
+    }
+}
