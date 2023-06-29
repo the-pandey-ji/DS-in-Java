758db149deaba49dec00430bd935c5a53d698607
the-pandey-ji <pandey143sonu@gmail.com>
2023-06-29 13:34:36 +0530

    done

--- Array.java
+++ Array.java
@@ -3,7 +3,7 @@ public class Array{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Array file testing");
-
+        System.out.println("testing");
         sc.close();
     }
 }
